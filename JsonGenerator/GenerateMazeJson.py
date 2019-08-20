8a42461b467ecca6ebcda10d4a8c1123681035ff
Jesper Huijgens <jesper.huijgens@sioux.eu>
2019-08-20 19:59:39 +0200

    Added mazeJson generator

---
+++ JsonGenerator/GenerateMazeJson.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+import json
+import random
+
+rows = 10
+columns = 10
+nTicks = 100
+ticks = []
+for tick in range(nTicks):
+    maze = []
+    for y in range(rows):
+        row = []
+        for x in range(columns):
+            row.append({
+                "type": random.randint(0, 1),
+                "orientation": random.randint(0,3)
+            })
+        maze.append(row)
+
+    actionTiles = []
+    nActionTiles = 5
+    for i in range(nActionTiles):
+        actionTiles.append({
+            "x": random.randint(0,9),
+            "y": random.randint(0,9),
+            "direction": random.choice(["left", "right", "up", "down"]),
+            "laneId": random.randint(0,9)
+        })
+        
+    nBots = 1
+    bots = []
+    for i in range(nBots):
+        bots.append({
+            "arucoId": i,
+            "position": [0.2 + (tick/(nTicks-1))*0.6, 0.6],
+            "forward": [0.02, -0.03],
+            "right": [-0.01, 0.04]
+        })
+
+    ticks.append({
+        "gameId": 1,
+        "gameTick": tick,
+        "rows": rows,
+        "columns": columns,
+        "data": maze,
+        "actionTiles": actionTiles,
+        "bots": bots
+    })
+
+for i, tick in enumerate(ticks, start=0):
+    with open("tick" + str(i) + ".json", 'w') as f:
+        json.dump(tick, f)
+        f.write("\n")

5671082642fc5fe5188151f29689bd0117f0e23f
Sri Muthu Narayanan Balasubramanian <muthu171191@gmail.com>
2019-09-26 07:16:02 +0200

    updated help text

--- BotTrackerService/BotTrackerSimulator.py
+++ BotTrackerService/BotTrackerSimulator.py
@@ -8,9 +8,9 @@ import GlobalConstants as gc
 
 def main():
     ap = argparse.ArgumentParser()
-    ap.add_argument("--server", type=str, default='localhost', help='server address')
-    ap.add_argument("--port", type=int, default=10000, help='server port')
-    ap.add_argument("--fps", type=int, default=20, help='data rate in fps for simulator')
+    ap.add_argument("--server", type=str, default='localhost', help='server address (default localhost)')
+    ap.add_argument("--port", type=int, default=10000, help='server port (default 10000)')
+    ap.add_argument("--fps", type=int, default=20, help='data rate in fps for simulator (default 20)')
     args = ap.parse_args()
 
     com_obj = com.Communicator(record_data=False, server_address=(args.server, args.port))
